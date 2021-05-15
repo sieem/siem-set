6ded70054f44b989abf5e3641c10c1d55fc71ba5
sieem <siem.linx@gmail.com>
2021-05-15 20:09:15 +0200

    When game is paused, don't start the timer

--- src/components/Timer/Timer.svelte
+++ src/components/Timer/Timer.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-    import { timeStore, controlTimer } from "../../services/Timer.service";
+    import { timeStore, controlTimer, timerStore } from "../../services/Timer.service";
     let time = 0;
+    let ticking = false;
+    let visibilityChangePausedTheGame = false;
 
-    document.addEventListener('visibilitychange', (ev) => controlTimer(document.visibilityState === 'visible'));
+    document.addEventListener('visibilitychange', (ev) => {
+        if (document.visibilityState === 'hidden' && ticking) {
+            controlTimer(false);
+            visibilityChangePausedTheGame = true;
+        }
+
+        if (document.visibilityState === 'visible' && visibilityChangePausedTheGame) {
+            controlTimer(true);
+            visibilityChangePausedTheGame = false;
+        }
+    });
 
     timeStore.subscribe((_time) => time = _time);
+    timerStore.subscribe((_ticking) => ticking = _ticking);
 </script>
 
 <div>{time}</div>
