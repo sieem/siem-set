c6169801e485149640b3a16e51a0f17eff53965c
sieem <siem.linx@gmail.com>
2023-09-23 17:40:44 +0200

    cardsRemaining fix

--- src/components/CardsRemaining/CardsRemaining.svelte
+++ src/components/CardsRemaining/CardsRemaining.svelte
@@ -23,6 +23,7 @@
     position: absolute;
     height: 100%;
     width: 100%;
+    cursor: auto;
   }
 
   .one {
--- src/services/Card.store.ts
+++ src/services/Card.store.ts
@@ -3,8 +3,9 @@ import { derived } from 'svelte/store';
 import type { ICard } from '../components/Card/ICard.interface';
 import { writeableWithValue } from '../helper/writeableWithValue.helper';
 
-export const amountOfCards = import.meta.env.MODE === 'production' ? 81 : 27;
+//@ts-expect-error env is not typed, check vite docs
+export const amountOfCards = import.meta.env.PROD ? 81 : 27;
 export const cards = writeableWithValue<ICard[]>([]);
 export const cardsOnTheTable = writeableWithValue<ICard[]>([]);
 export const activatedCards = writeableWithValue<ICard[]>([]);
-export const cardsRemaining = derived(cardsOnTheTable, (cardsOnTheTable) => cardsOnTheTable.length);
+export const cardsRemaining = derived(cards, (cards) => cards.length);
